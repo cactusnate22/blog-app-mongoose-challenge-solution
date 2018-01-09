491d70db65c45831a5e64b9a995228f56a9d1ba4
Nathan Ash <nash311@gmail.com>
2018-01-09 14:32:42 -0500

    initial commit- modified server.js and config.js, added test-blog-app.js

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ const app = express();
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
-app.get('/posts', (req, res) => {
+app.get('/blog-posts', (req, res) => {
   BlogPost
     .find()
     .then(posts => {
-      res.json(posts.map(post => post.serialize()));
+      res.json({
+        posts: posts.map(
+          (post) => post.serialize())
+        });
     })
     .catch(err => {
       console.error(err);
@@ -26,7 +29,7 @@ app.get('/posts', (req, res) => {
     });
 });
 
-app.get('/posts/:id', (req, res) => {
+app.get('/blog-posts/:id', (req, res) => {
   BlogPost
     .findById(req.params.id)
     .then(post => res.json(post.serialize()))
@@ -36,8 +39,8 @@ app.get('/posts/:id', (req, res) => {
     });
 });
 
-app.post('/posts', (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
+app.post('/blog-posts', (req, res) => {
+  const requiredFields = ['author', 'title', 'content'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -49,11 +52,11 @@ app.post('/posts', (req, res) => {
 
   BlogPost
     .create({
+      author: req.body.author,
       title: req.body.title,
-      content: req.body.content,
-      author: req.body.author
+      content: req.body.content
     })
-    .then(blogPost => res.status(201).json(blogPost.serialize()))
+    .then(post => res.status(201).json(post.serialize()))
     .catch(err => {
       console.error(err);
       res.status(500).json({ error: 'Something went wrong' });
@@ -61,21 +64,7 @@ app.post('/posts', (req, res) => {
 
 });
 
-
-app.delete('/posts/:id', (req, res) => {
-  BlogPost
-    .findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(204).json({ message: 'success' });
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
-    });
-});
-
-
-app.put('/posts/:id', (req, res) => {
+app.put('/blog-posts/:id', (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     res.status(400).json({
       error: 'Request path id and request body id values must match'
@@ -97,15 +86,20 @@ app.put('/posts/:id', (req, res) => {
 });
 
 
-app.delete('/:id', (req, res) => {
+app.delete('/blog-posts/:id', (req, res) => {
   BlogPost
     .findByIdAndRemove(req.params.id)
-    .then(() => {
-      console.log(`Deleted blog post with id \`${req.params.ID}\``);
-      res.status(204).end();
-    });
+    .then(post => res.status(204).end())
+    .catch(err => res.status(204).json({ message: 'Internal server error' }));
 });
 
+// app.delete('/:id', (req, res) => {
+//   BlogPost
+//     .findByIdAndRemove(req.params.id)
+//     .then(() => {
+//       console.log(`Deleted blog post with id \`${req.params.ID}\``);
+//       res.status(204).end();
+//     });
 
 app.use('*', function (req, res) {
   res.status(404).json({ message: 'Not Found' });
